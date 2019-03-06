1c30371d0c8261a994672db2b3f528f3b35869f1
Jacqueline <jacq@in-the-box.nl>
2019-03-06 16:46:19 +0100

    Update install.sh

--- kubernetes-cluster/install.sh
+++ kubernetes-cluster/install.sh
@@ -25,7 +25,7 @@ echo "\nDeploying kubernetes with the Flannel network overlay\n"
 kubeadm init --pod-network-cidr=10.244.0.0/16
 export KUBECONFIG=/etc/kubernetes/admin.conf
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
-touch /tmp/install
+touch /tmp/installed
 else
         echo "It looks like you installed Kubernetes already"
 fi
